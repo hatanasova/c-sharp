fcb138a494f6fa14d7b7bce3a53eb8c3e7fed334
Hristina <hristina.atanasova6@gmail.com>
2020-03-30 22:51:19 +0200

    Homework02_Task4

---
+++ Homework_02/Homework2_Task4/Homework2_Task4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Homework2_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SimpleCalc!\nEnter two numbers and an operand.\n");
+
+            Console.Write("First number: ");
+            string firstNumber = Console.ReadLine();
+
+            Console.Write("Second number: ");
+            string secondtNumber = Console.ReadLine();
+
+            Console.Write("Choose the operand ('+', '-', '*' or '/'): ");
+            string operand = Console.ReadLine();
+
+            SimpleCalc(firstNumber, secondtNumber, operand);
+        }
+
+        static void SimpleCalc(string sNum1, string sNum2, string operand)
+        {
+            double number1, number2;
+            if ( !(Double.TryParse(sNum1, out number1) && Double.TryParse(sNum2, out number2)))
+            {
+                Console.WriteLine("Input not a number!");
+            } else
+            {
+                switch (operand)
+                {
+                    case "+":
+                        Console.WriteLine(number1 + " + " + number2 + " = " + (number1 + number2));
+                        break;
+                    case "-":
+                        Console.WriteLine(number1 + " - " + number2 + " = " + (number1 - number2));
+                        break;
+                    case "*":
+                        Console.WriteLine(number1 + " * " + number2 + " = " + (number1 * number2));
+                        break;
+                    case "/":
+                        Console.WriteLine(number1 + " / " + number2 + " = " + (number1 / number2));
+                        break;
+                    default:
+                        Console.WriteLine("Something Went Wrong! Try Again!");
+                        break;
+                }
+            }
+        }
+    }
+}
