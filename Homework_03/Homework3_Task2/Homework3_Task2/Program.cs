cd8472df1e921e0c9b19833150f4c3603da02067
Hristina <hristina.atanasova6@gmail.com>
2020-03-30 23:33:54 +0200

    Homework3, Task2

---
+++ Homework_03/Homework3_Task2/Homework3_Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Homework3_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("To check if a triangle is valid, enter the length of the three sides: ");
+
+            Console.Write("Length of side one: ");
+            string sideOne = Console.ReadLine();
+
+            Console.Write("Length of side two: ");
+            string sideTwo = Console.ReadLine();
+
+            Console.Write("Length of side three: ");
+            string sideThree = Console.ReadLine();
+
+            if (CheckTriangleValidity(sideOne,sideTwo, sideThree) == 1)
+                Console.WriteLine("The triangle is valid.");
+            else
+                Console.WriteLine("The triangle is not valid.");
+        }
+
+        static int CheckTriangleValidity(string a, string b, string c)
+        {
+            double sideOne, sideTwo, sideThree;
+            Double.TryParse(a, out sideOne);
+            Double.TryParse(b, out sideTwo);
+            Double.TryParse(c, out sideThree);
+
+            if (sideOne + sideTwo < sideThree || sideOne + sideThree < sideTwo || sideTwo + sideThree < sideOne)
+                return 0;
+            else
+                return 1;
+        }
+    }
+}
