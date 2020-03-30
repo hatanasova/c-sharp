6a4a8925401137599a1ba79bd2d141345ea17caa
Hristina <hristina.atanasova6@gmail.com>
2020-03-30 20:39:09 +0200

    Initial commit

---
+++ Homework_02/Homework2_Task1/Homework2_Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CollectApples();
+        }
+
+        static void CollectApples()
+        {
+            int nApples = 22;
+            int mBaskets = 5;
+
+            Console.WriteLine("We need " + Math.Ceiling((double)nApples / mBaskets) + " baskets to collect " + nApples);
+        }
+    }
+}
